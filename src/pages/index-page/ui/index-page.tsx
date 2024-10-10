3cd11f2c6013a1312d44a449ede0ab571683cebc
moverq1337 <moverq1337@gmail.com>
2024-10-10 17:44:06 +0300

    fixed

--- src/pages/index-page/ui/index-page.tsx
+++ src/pages/index-page/ui/index-page.tsx
@@ -1,12 +1,26 @@
+import { Button } from '@nextui-org/button'
+import { MdKeyboardArrowRight } from 'react-icons/md'
 import { MainLayout } from 'src/layout'
 
 const IndexPage: React.FC = () => {
 	return (
 		<MainLayout>
-			<div className='flex justify-center pt-52'>
-				<h1 className='font-customFont text-landing text-mainGreen px-6'>
+			<div className='flex items-center flex-col pt-36 gap-10'>
+				<h1 className='font-customFont text-landing text-mainGreen pt-6'>
 					МОЁ ИСКУССТВО!
 				</h1>
+				<p className='text-mainGreen text-xl'>
+					Сервис для поиска персонала и сообщества по интересам
+				</p>
+				<Button className='bg-mainGreen px-12 w-fit text-black'>
+					Создать
+					<MdKeyboardArrowRight />
+				</Button>
+				{/* <div className='flex justify-center'>
+					<Button className='max-w-3xl flex justify-center' color='primary'>
+						Button
+					</Button>
+				</div> */}
 			</div>
 		</MainLayout>
 	)
