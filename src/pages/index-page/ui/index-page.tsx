1fba19f8d6711af691a75360a1516b42e9680774
unkabas <undercannabis10@gmail.com>
2024-10-10 07:08:44 -0700

    header add

--- src/pages/index-page/ui/index-page.tsx
+++ src/pages/index-page/ui/index-page.tsx
@@ -3,7 +3,7 @@ import { MainLayout } from 'src/layout'
 const IndexPage: React.FC = () => {
 	return (
 		<MainLayout>
-			<h1 className='font-customFont text-landing text-mainGreen'>
+			<h1 className='font-customFont text-landing text-mainGreen px-6'>
 				МОЁ ИСКУССТВО!
 			</h1>
 		</MainLayout>
--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -1,9 +1,16 @@
 const Header: React.FC = () => {
-    return (
-        <header>
-            
-        </header>
-    )
+	return (
+		<header>
+			<div className='flex justify-between items-center text-header text-mainGreen font-roboto'>
+				<div className='flex gap-28 '>
+					<button>LOGO</button>
+					<button>Мои интересы</button>
+					<button>Чаты</button>
+				</div>
+				<button>Профиль</button>
+			</div>
+		</header>
+	)
 }
 
 export default Header
